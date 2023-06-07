dfaca8a5346a8ca42e99b33371503481018faf97
eneskarateke <eneskarateke@gmail.com>
2023-06-07 18:04:00 +0300

    styling

--- src/Components/NightModeToggle.js
+++ src/Components/NightModeToggle.js
@@ -3,8 +3,6 @@ import React, { useContext } from "react";
 import "./CSS/NightMode.css";
 import { NightModeContext } from "../NightModeContext";
 
-import DarkModeToggle from "react-dark-mode-toggle";
-
 function NightModeToggle() {
   const { nightMode, setNightMode } = useContext(NightModeContext);
 
@@ -13,16 +11,22 @@ function NightModeToggle() {
   };
 
   return (
-    <div className="darkModeContainer">
-      <DarkModeToggle
-        onChange={toggleNightMode}
-        checked={nightMode}
-        size={50}
-      />
+    <>
+      <div className="darkModeContainer">
+        <label className="toggle-switch">
+          <input
+            type="checkbox"
+            className="toggle-switch-checkbox"
+            onChange={toggleNightMode}
+            checked={nightMode}
+          />
+          <span className="slider round"></span>
+        </label>
+      </div>
       <p onClick={toggleNightMode} className="darkModeText">
         {nightMode ? "LIGHT MODE" : "DARK MODE"}
       </p>
-    </div>
+    </>
   );
 }
 
